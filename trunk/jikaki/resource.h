3bbb257be4d27c5cd91dc20aa7c3a12bb494d8fc
ku@digitaldolphins.jp <ku@digitaldolphins.jp@e5e4a8b0-fbfa-11de-aa5a-5b26b85a8715>
2010-03-16 02:48:15 +0000

    jikaki 1.0

---
+++ trunk/jikaki/resource.h
@@ -0,0 +1,21 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by A.rc
+//
+#define IDR_TEMPLATE_RTF                101
+#define IDR_TOPMENU                     102
+#define IDI_ICON1                       103
+#define ID_NEW                          40001
+#define ID_LOAD                         40002
+#define ID_SAVE                         40003
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        104
+#define _APS_NEXT_COMMAND_VALUE         40004
+#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif

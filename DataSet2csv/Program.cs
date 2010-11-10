95eea887892ad3e8461dd178c11bc3de1ff88acb
ku <ku@digitaldolphins.jp>
2010-11-10 01:00:21 +0000

    DataSet2csv 1.0.0

---
+++ DataSet2csv/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace DataSet2csv {
+    static class Program {
+        /// <summary>
+        /// アプリケーションのメイン エントリ ポイントです。
+        /// </summary>
+        [STAThread]
+        static void Main() {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new CForm());
+        }
+    }
+}

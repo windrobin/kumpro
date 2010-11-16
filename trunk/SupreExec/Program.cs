8d8c4639ee3a710771829696fe908f389f0b7de9
ku@digitaldolphins.jp <ku@digitaldolphins.jp@e5e4a8b0-fbfa-11de-aa5a-5b26b85a8715>
2010-11-16 02:32:30 +0000

    SupreExec 1.0

---
+++ trunk/SupreExec/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+using Microsoft.Win32;
+using System.IO;
+using System.Diagnostics;
+
+namespace SupreExec {
+    static class Program {
+        [MTAThread()]
+        static void Main(string[] args) {
+            String app = "", cmd = "";
+
+            foreach (string arg in args) {
+                Match M;
+                if ((M = Regex.Match(arg, "^\\*(?<env>[^=]+)=(?<path>(.+))$")).Success) {
+                    String path = M.Groups["path"].Value;
+                    String env = Convert.ToString(Registry.GetValue(Path.GetDirectoryName(path), Path.GetFileName(path), ""));
+
+                    Environment.SetEnvironmentVariable(M.Groups["env"].Value, env);
+                }
+                else if (String.IsNullOrEmpty(app)) {
+                    app = Environment.ExpandEnvironmentVariables(arg);
+                }
+                else {
+                    cmd += " \"" + Environment.ExpandEnvironmentVariables(arg) + "\"";
+                }
+            }
+
+            //MessageBox.Show(app + "\n" + cmd);
+
+            ProcessStartInfo psi = new ProcessStartInfo(app, cmd);
+            Process.Start(psi);
+        }
+    }
+}

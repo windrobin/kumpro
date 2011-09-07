48cf245a33ba32ed80e2082739bd36c1e405a417
ku <ku@digitaldolphins.jp>
2011-09-07 05:32:14 +0000

    EditMdbLink 0.1

---
+++ EditMdbLink/resource.h
@@ -0,0 +1,25 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by EditMdbLink.rc
+//
+#define IDD_EDITMDBLINK_DIALOG          102
+#define IDR_MAINFRAME                   128
+#define IDS_CONN_UPDATED                129
+#define IDS_CONN_UPDATE_NOT_REQUIRED    130
+#define ID_QUERY_SAVE                   131
+#define IDC_EDIT_MDB                    1000
+#define IDC_REF_MDB                     1001
+#define IDC_OPEN                        1002
+#define IDC_CONNS                       1004
+#define IDC_SAVE                        1005
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        130
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1007
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif

6730cfb6789899649327e9d6deafd0634589e7bb
ku <ku@digitaldolphins.jp>
2011-12-21 00:44:12 +0000

    IEView 0.1

---
+++ IEView/UrlDlg.h
@@ -0,0 +1,23 @@
+﻿#pragma once
+
+
+// CUrlDlg ダイアログ
+
+class CUrlDlg : public CDialog
+{
+	DECLARE_DYNAMIC(CUrlDlg)
+
+public:
+	CUrlDlg(CWnd* pParent = NULL);   // 標準コンストラクタ
+	virtual ~CUrlDlg();
+
+// ダイアログ データ
+	enum { IDD = IDD_URLDLG };
+
+protected:
+	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV サポート
+
+	DECLARE_MESSAGE_MAP()
+public:
+	CString m_strUrl;
+};

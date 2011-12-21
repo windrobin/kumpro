fe8d23e6990ab9bf5b1627cd84501094f335f031
ku@digitaldolphins.jp <ku@digitaldolphins.jp@e5e4a8b0-fbfa-11de-aa5a-5b26b85a8715>
2011-12-21 00:44:12 +0000

    IEView 0.1

---
+++ trunk/IEView/IEViewView.cpp
@@ -0,0 +1,100 @@
+﻿// IEViewView.cpp : CVw クラスの実装
+//
+
+#include "stdafx.h"
+#include "IEView.h"
+
+#include "IEViewDoc.h"
+#include "IEViewView.h"
+
+#include "UrlDlg.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// CVw
+
+IMPLEMENT_DYNCREATE(CVw, CHtmlView)
+
+BEGIN_MESSAGE_MAP(CVw, CHtmlView)
+	// 標準印刷コマンド
+	ON_COMMAND(ID_FILE_PRINT, &CHtmlView::OnFilePrint)
+	ON_COMMAND(ID_FILE_URL, &CVw::OnFileUrl)
+END_MESSAGE_MAP()
+
+// CVw コンストラクション/デストラクション
+
+CVw::CVw()
+{
+	// TODO: 構築コードをここに追加します。
+
+}
+
+CVw::~CVw()
+{
+}
+
+BOOL CVw::PreCreateWindow(CREATESTRUCT& cs)
+{
+	// TODO: この位置で CREATESTRUCT cs を修正して Window クラスまたはスタイルを
+	//  修正してください。
+
+	return CHtmlView::PreCreateWindow(cs);
+}
+
+extern CString g_strUrl;
+
+void CVw::OnInitialUpdate()
+{
+	CHtmlView::OnInitialUpdate();
+
+	Navigate2(g_strUrl,NULL,NULL);
+}
+
+
+// CVw 印刷
+
+
+
+// CVw 診断
+
+#ifdef _DEBUG
+void CVw::AssertValid() const
+{
+	CHtmlView::AssertValid();
+}
+
+void CVw::Dump(CDumpContext& dc) const
+{
+	CHtmlView::Dump(dc);
+}
+
+CDoc* CVw::GetDocument() const // デバッグ以外のバージョンはインラインです。
+{
+	ASSERT(m_pDocument->IsKindOf(RUNTIME_CLASS(CDoc)));
+	return (CDoc*)m_pDocument;
+}
+#endif //_DEBUG
+
+// CVw メッセージ ハンドラ
+
+void CVw::OnFileUrl() {
+	CUrlDlg wndDlg(this);
+	wndDlg.m_strUrl = GetLocationURL();
+	if (wndDlg.DoModal() == IDOK) {
+		Navigate2(g_strUrl = wndDlg.m_strUrl,NULL,NULL);
+	}
+}
+
+void CVw::OnStatusTextChange(LPCTSTR lpszText) {
+	CHtmlView::OnStatusTextChange(lpszText);
+}
+
+void CVw::OnNavigateComplete2(LPCTSTR strURL)
+{
+	// TODO: ここに特定なコードを追加するか、もしくは基本クラスを呼び出してください。
+
+	CHtmlView::OnNavigateComplete2(strURL);
+}
---
+++ trunk/IEView/IEView_i.c
@@ -0,0 +1,85 @@
+
+
+/* this ALWAYS GENERATED file contains the IIDs and CLSIDs */
+
+/* link this file in with the server and any clients */
+
+
+ /* File created by MIDL compiler version 7.00.0555 */
+/* at Wed Dec 21 09:43:22 2011
+ */
+/* Compiler settings for .\IEView.idl:
+    Oicf, W1, Zp8, env=Win32 (32b run), target_arch=X86 7.00.0555 
+    protocol : dce , ms_ext, c_ext
+    error checks: allocation ref bounds_check enum stub_data 
+    VC __declspec() decoration level: 
+         __declspec(uuid()), __declspec(selectany), __declspec(novtable)
+         DECLSPEC_UUID(), MIDL_INTERFACE()
+*/
+/* @@MIDL_FILE_HEADING(  ) */
+
+#pragma warning( disable: 4049 )  /* more than 64k source lines */
+
+
+#ifdef __cplusplus
+extern "C"{
+#endif 
+
+
+#include <rpc.h>
+#include <rpcndr.h>
+
+#ifdef _MIDL_USE_GUIDDEF_
+
+#ifndef INITGUID
+#define INITGUID
+#include <guiddef.h>
+#undef INITGUID
+#else
+#include <guiddef.h>
+#endif
+
+#define MIDL_DEFINE_GUID(type,name,l,w1,w2,b1,b2,b3,b4,b5,b6,b7,b8) \
+        DEFINE_GUID(name,l,w1,w2,b1,b2,b3,b4,b5,b6,b7,b8)
+
+#else // !_MIDL_USE_GUIDDEF_
+
+#ifndef __IID_DEFINED__
+#define __IID_DEFINED__
+
+typedef struct _IID
+{
+    unsigned long x;
+    unsigned short s1;
+    unsigned short s2;
+    unsigned char  c[8];
+} IID;
+
+#endif // __IID_DEFINED__
+
+#ifndef CLSID_DEFINED
+#define CLSID_DEFINED
+typedef IID CLSID;
+#endif // CLSID_DEFINED
+
+#define MIDL_DEFINE_GUID(type,name,l,w1,w2,b1,b2,b3,b4,b5,b6,b7,b8) \
+        const type name = {l,w1,w2,{b1,b2,b3,b4,b5,b6,b7,b8}}
+
+#endif !_MIDL_USE_GUIDDEF_
+
+MIDL_DEFINE_GUID(IID, LIBID_IEView,0xFE4D822C,0x9BFC,0x451E,0x94,0x99,0xDA,0x5D,0xC9,0xFF,0x3B,0x1E);
+
+
+MIDL_DEFINE_GUID(IID, DIID_IIEView,0x30057643,0xDF70,0x4AC3,0xBF,0x00,0x1C,0xE1,0x19,0x2D,0xF3,0x09);
+
+
+MIDL_DEFINE_GUID(CLSID, CLSID_CDoc,0x6B8B1B23,0xFE04,0x4308,0x99,0x9A,0xB5,0xE7,0x06,0x4E,0x84,0x9F);
+
+#undef MIDL_DEFINE_GUID
+
+#ifdef __cplusplus
+}
+#endif
+
+
+

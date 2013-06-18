46f9cdbc9b67d0b4ec692ddd62c4440570a8f5e1
ku <ku@digitaldolphins.jp>
2013-06-18 02:30:41 +0000

    CGI4Ruby/1.1

--- CGItest/CGI4Ruby/Properties/AssemblyInfo.cs
+++ CGItest/CGI4Ruby/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("CGI4Ruby")]
-[assembly: AssemblyCopyright("Copyright (C)  2010")]
+[assembly: AssemblyCopyright("Copyright (C)  2010,2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly:     AssemblyVersion("1.1.0.0")]//here
+[assembly: AssemblyFileVersion("1.1.0.0")]//here
